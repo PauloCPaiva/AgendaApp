c71eab370168ea5cb45ba832570adcbb24644bbe
PauloC.Jr <paulocesarapjr@gmail.com>
2025-07-31 22:08:18 -0300

    Modelagem e mapeamento das entidades, configuração do Entity Framework e Migrations para cirar o banco de dados.

---
+++ AgendaApp.API/Contexts/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using AgendaApp.API.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaApp.API.Contexts
+{
+    /// <summary>
+    /// Classe para configuração do Entity Framework Core e acesso ao banco de dados.
+    /// </summary>
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Configuração do provedor e o caminho do banco de dados.
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BDAgenda;Integrated Security=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Adicionar cada classe de mapeamento feita no projeto.
+            modelBuilder.ApplyConfiguration(new CategoriaMap());
+            modelBuilder.ApplyConfiguration(new TarefaMap());
+        }
+    }
+}
+
+
+
---
+++ AgendaApp.API/Enums/Prioridade.cs
@@ -0,0 +1,12 @@
+﻿namespace AgendaApp.API.Enums
+{
+    /// <summary>
+    /// Lista de opcões para cadastro de prioridades.
+    /// </summary>
+    public enum Prioridade
+    {
+        Alta = 1,
+        Media = 2,
+        Baixa = 3
+    }
+}
